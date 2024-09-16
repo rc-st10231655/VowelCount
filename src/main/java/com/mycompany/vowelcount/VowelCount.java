191c517e4716227cc6950cd7fe8b2bc9f70f6094
rc-st10231655 <st10231655@rcconnect.edu.za>
2024-09-16 10:29:12 +0200

    Added comments

--- src/main/java/com/mycompany/vowelcount/VowelCount.java
+++ src/main/java/com/mycompany/vowelcount/VowelCount.java
@@ -10,33 +10,41 @@ import java.util.Scanner;
  *
  * @author ST10231655
  */
+// Vowel count class
 public class VowelCount {
 
+    // Main method
     public static void main(String[] args) {
+        // Declare scanner object
         Scanner scannerObj = new Scanner(System.in);
 
+        // Prompt user to enter a sentence
         System.out.print("Enter a sentence: ");
-        String sentence = scannerObj.nextLine();
+        // User enters a sentence
+        String sentence = scannerObj.nextLine(); // Sentence string
 
-        int vowelsNum = countVowels(sentence);
+        int vowelsNum = countVowels(sentence); // Vowels number integer
 
-        System.out.println("Number of vowels in sentence: " + vowelsNum);
+        System.out.println("Number of vowels in sentence: " + vowelsNum); // Display vowels number
     }
 
+    // Count vowels method
     public static int countVowels(String sentence) {
-        int vowelCount = 0;
-        String vowels = "aeiou";
+        int vowelCount = 0; // Vowel count integer
+        String vowels = "aeiou"; // Vowels string
 
-        sentence = sentence.toLowerCase();
+        sentence = sentence.toLowerCase(); // Convert sentence to lowercase
 
+        // Loop characters in the sentence
         for (int i = 0; i < sentence.length(); i++) {
-            char senChar = sentence.charAt(i);
+            char senChar = sentence.charAt(i); // Sentence character
+            // Check if character is a vowel
             if (vowels.indexOf(senChar) != -1) {
-                vowelCount++;
+                vowelCount++; // Increment vowel count
             }
         }
 
-        return vowelCount;
+        return vowelCount; // Return vowel count
     }
 }
 
